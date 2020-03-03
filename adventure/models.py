e7c0e9c9c035dccd3effe8011d706bc2f77c2637
Vyraal1 <kevin.afable@gmail.com>
2020-03-03 17:03:17 -0500

    feat: rooms delete on cascade

--- adventure/models.py
+++ adventure/models.py
@@ -10,16 +10,14 @@ class Room(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(
         max_length=500, default="DEFAULT DESCRIPTION")
-    north = models.IntegerField(default=0)
-    south = models.IntegerField(default=0)
-    east = models.IntegerField(default=0)
-    west = models.IntegerField(default=0)
+    north = models.IntegerField(default=None, null=True)
+    south = models.IntegerField(default=None, null=True)
+    east = models.IntegerField(default=None, null=True)
+    west = models.IntegerField(default=None, null=True)
     x = models.IntegerField(default=0)
     y = models.IntegerField(default=0)
-    # items = models.ManyToManyField()
 
     def connectRooms(self, destination_room, direction):
-        destination_id = destination_room.id
         opposite = {
             'north': 'south',
             'south': 'north',
@@ -27,34 +25,43 @@ class Room(models.Model):
             'west': 'east'
         }
         try:
-            destination = Room.objects.get(id=destination_id)
+            destination = Room.objects.get(id=destination_room.id)
         except Room.DoesNotExist:
             print("That room does not exist")
         else:
             setattr(self, direction, destination)
+            setattr(destination, opposite[direction], self)
             return
+        destination.save()
         self.save()
 
+    # gets the player name with the given ID
     def playerNames(self, currentPlayerID):
-        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+        return [p.user.username for p in Player.objects.filter(currentRoom=self.id)
+                if p.id != int(currentPlayerID)]
 
+    # what is the player UUID?
     def playerUUIDs(self, currentPlayerID):
-        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+        return [p.uuid for p in Player.objects.filter(currentRoom=self.id)
+                if p.id != int(currentPlayerID)]
 
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    currentRoom = models.IntegerField(default=0)
+    # currentRoom = models.IntegerField(default=0)
+    current_room = models.ForeignKey(
+        Room, on_delete=models.CASCADE, null=True, blank=True)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
 
     def initialize(self):
-        if self.currentRoom == 0:
-            self.currentRoom = Room.objects.first().id
+        if self.current_room == 0:
+            self.current_room = Room.objects.first().id
             self.save()
 
+    # if the room was deleted, put the player back in start
     def room(self):
         try:
-            return Room.objects.get(id=self.currentRoom)
+            return Room.objects.get(id=self.current_room)
         except Room.DoesNotExist:
             self.initialize()
             return self.room()
