9392d959d373fe792c8283685b0c3f9c2cb9c2d6
Vyraal1 <kevin.afable@gmail.com>
2020-03-03 21:09:59 -0500

    feat: made rooms rooms instead of ID's

--- adventure/models.py
+++ adventure/models.py
@@ -10,14 +10,23 @@ class Room(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(
         max_length=500, default="DEFAULT DESCRIPTION")
-    north = models.IntegerField(default=None, null=True)
-    south = models.IntegerField(default=None, null=True)
-    east = models.IntegerField(default=None, null=True)
-    west = models.IntegerField(default=None, null=True)
+    # north = models.IntegerField(default=None, null=True)
+    # south = models.IntegerField(default=None, null=True)
+    # east = models.IntegerField(default=None, null=True)
+    # west = models.IntegerField(default=None, null=True)
+    north = models.ForeignKey(
+        'self', related_name="north_exit", on_delete=models.CASCADE, null=True)
+    south = models.ForeignKey(
+        'self', related_name="south_exit", on_delete=models.CASCADE, null=True)
+    west = models.ForeignKey(
+        'self', related_name="west_exit", on_delete=models.CASCADE, null=True)
+    east = models.ForeignKey(
+        'self', related_name="east_exit", on_delete=models.CASCADE, null=True)
     x_cor = models.IntegerField(default=0)
     y_cor = models.IntegerField(default=0)
 
     def connectRooms(self, destination_room, direction):
+        print(f'initializing connection from {self} to {destination_room}')
         opposite = {
             'north': 'south',
             'south': 'north',
@@ -26,14 +35,14 @@ class Room(models.Model):
         }
         try:
             destination = Room.objects.get(id=destination_room.id)
+            print(f'successfully got destination at {destination}')
         except Room.DoesNotExist:
             print("That room does not exist")
         else:
             setattr(self, direction, destination)
             setattr(destination, opposite[direction], self)
-            return
-        destination.save()
-        self.save()
+            self.save()
+            print(f'successfully connected {self} to {destination}')
 
     # gets the player name with the given ID
     def playerNames(self, currentPlayerID):
